9c52efe09928c62085ff9fa37223174853fce80a
Borislav Aleksandrov 1906490 <bork0047@abv.bg>
2023-04-06 23:23:04 +0100

    Made the server, installed express and ran everything

--- PriceCalc/backend/app.js
+++ PriceCalc/backend/app.js
@@ -1,13 +1,31 @@
 const http = require('http')
 const port = 8080
+const fs = require('fs')
+var express = require('express')
+
 var currentdate = require('./mydate');
+var knockknock = require('knock-knock-jokes');
+
 
 const server = http.createServer(function(req, res){
-    res.write('Hello Node')
-    res.write("The date and time are currently: " + currentdate.myDateTime());
-    res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'} );
+    fs.readFile('C:/Users/USER/Desktop/EntWebSys/GITHUB-REPOSITORY/CM4025-Enterprise-Web-Development/PriceCalc/frontend/myWebPage.html', function(error, data) {
+        if(error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        } 
+        res.end()
+    })
 })
 
+
+//res.write("The date and time are currently: " + currentdate.myDateTime());
+    //res.write( " \n")
+    //var randomJoke = knockknock()
+    //res.end(randomJoke);
+    
 server.listen(port, function(error){
     if (error) {
         console.log('Something went wrong', error)
