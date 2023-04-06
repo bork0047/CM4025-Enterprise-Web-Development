ea388312ad0a6266fb0874ed19054754a1f14fa2
Borislav Aleksandrov 1906490 <bork0047@abv.bg>
2023-04-07 00:02:00 +0100

    More changes

--- PriceCalc/backend/app.js
+++ PriceCalc/backend/app.js
@@ -2,31 +2,20 @@ const http = require('http')
 const port = 8080
 const fs = require('fs')
 var express = require('express')
+var path = require('path')
+var app = express()
 
-var currentdate = require('./mydate');
-var knockknock = require('knock-knock-jokes');
 
-
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'} );
-    fs.readFile('C:/Users/USER/Desktop/EntWebSys/GITHUB-REPOSITORY/CM4025-Enterprise-Web-Development/PriceCalc/frontend/myWebPage.html', function(error, data) {
-        if(error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        } 
-        res.end()
-    })
+app.get("/", function(req, res) {
+    res.send('Index route is working')
 })
-
 
 //res.write("The date and time are currently: " + currentdate.myDateTime());
     //res.write( " \n")
     //var randomJoke = knockknock()
     //res.end(randomJoke);
     
-server.listen(port, function(error){
+app.listen(port, function(error){
     if (error) {
         console.log('Something went wrong', error)
     } else {
