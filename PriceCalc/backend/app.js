ccd7897baeb3183a5b1b74344e069933f18abb21
Borislav Aleksandrov 1906490 <bork0047@abv.bg>
2023-04-07 22:29:39 +0100

    CHanges to file locations

--- PriceCalc/backend/app.js
+++ PriceCalc/backend/app.js
@@ -1,9 +1,62 @@
+//imports
 const http = require('http')
 const port = 8080
 const fs = require('fs')
-var express = require('express')
-var path = require('path')
-var app = express()
+const express = require('express')
+const app = express()
+
+app.use(express.static('frontendd'))
+app.use('/css', express.static(__dirname + 'frontendd/css'))
+app.use('/html', express.static(__dirname + 'frontendd/html'))
+app.use('/images', express.static(__dirname + 'frontendd/images'))
+
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/PriceCalc/frontendd/html/myWebPage.html')
+})
+
+/*
+//json parser
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    if(req.headers.token == 'NODEJSAPP') {
+        req.skip == true;
+    } else {
+        req.skip = false;
+    }
+    next();
+});
+
+app.route('/auth').post((req, res) => {
+    if(req.skip){
+        res.send('OK, authenticated!')
+    } else {
+        res.send("No, can t access that");
+    }
+});
+
+app.route('/').get((req, res) => {
+    res.send('Hello World!');
+});
+*/
+app.listen(port, () => {
+    console.log('Server Started on port: ' + port);
+});
+
+/*
+//connects the express-handlebard package and tells it to go look
+//into the frontendd folder for the html files
+app.set("frontendd", path.join(__dirname, "frontendd"))
+app.engine(".hbs", exphbs({
+    extname:".hbs",
+    defaultLayout: false
+}))
+app.set("view engine", ".hbs")
 
 
 app.get("/", function(req, res) {
@@ -22,3 +75,5 @@ app.listen(port, function(error){
         console.log('Server is listening on port ' + port)
     }
 })
+
+*/
