76dfe89234868792b3320418e68e1e3adf2590c9
Borislav Aleksandrov 1906490 <bork0047@abv.bg>
2023-04-11 21:56:26 +0100

    Still mongo time

--- app.js
+++ app.js
@@ -15,20 +15,25 @@ const uri = "mongodb://127.0.0.1:27017"
 const client = new MongoClient(uri);
 async function run() {
 try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-    console.log('Start the database stuff');
     //Write databse Insert/Update/Query code here..
- console.log('End the database stuff');
+     var dbo = client.db("mydb");
+    var myobj = { quoteName: n, salary: s, days: d }; //******CHECK!!!****
+    await dbo.collection("quotes").insertOne(myobj, function(err, res) {
+        if (err) {
+            console.log(err); 
+            throw err;
+        }
+        console.log("1 quote inserted");
+    }); 
+    console.log('End the database stuff');
+
 } finally {
-// Ensures that the client will close when you finish/error
+    // Ensures that the client will close when you finish/error
     await client.close();
 }
 }
 run().catch(console.dir);
+
 
 
 
