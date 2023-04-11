dbd95544448249fd17e5e355b8ae0591101f6a2c
Borislav Aleksandrov 1906490 <bork0047@abv.bg>
2023-04-12 00:52:09 +0100

    Small changes

--- app.js
+++ app.js
@@ -6,38 +6,90 @@ const express = require('express')
 const app = express()
 
 //mongo time
-const { MongoClient } = require("mongodb")
-const uri = "mongodb://127.0.0.1:27017"
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://test:password@127.0.0.1:27017/quotes";
 
 
 var options = {
     index: "index.ejs"
   };
 
-//code for connecting to Mongo Database
-// Database stuff
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-try {
-    //Write databse Insert/Update/Query code here..
-     var dbo = client.db("DATAbased");
-    var myobj = { quoteName: n, salary: s, days: d }; //******CHECK!!!****
-    await dbo.collection("quotes").insertOne(myobj, function(err, res) {
-        if (err) {
-            console.log(err); 
-            throw err;
-        }
-        console.log("1 quote inserted");
-    }); 
-    console.log('End the database stuff');
 
-} finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-}
-}
-run().catch(console.dir);
+
+app.get('/api', function(req, res){
+    res.send("Yes we have an API now")
+})
+
+// e.g. test using:
+//http://127.0.0.1:8000/api/getPrice?salary=2000&days=20
+app.get('/api/getPrice', function(req, res){
+    //res.send("Hello world!")
+    // Copied from front end
+    var s = req.query.salary;
+    var d = req.query.days;
+    console.log("Calculating price")
+    console.log(s)
+    console.log(d)
+    let finalPrice = 0;
+    dailyRate = s/365;
+    price = Math.round(dailyRate * d);
+    var roundToNearest = 50;
+    roundedPrice = Math.round((price+roundToNearest)/roundToNearest) * roundToNearest // Always round up
+    res.send(""+roundedPrice)
+})
+
+
+
+
+
+//code for connecting to Mongo Database
+app.get('/api/storeQuote', function(req, res){
+    //res.send("Hello world!")
+    // Copied from front end
+    var n = req.query.quoteName
+    var s = req.query.salary;
+    var d = req.query.days;
+    console.log("Storing quote: "+n+" "+s+" "+d)
+    console.log("Mongo URI is "+uri)
+
+    // Database stuff
+    // Create a new MongoClient
+    const client = new MongoClient(uri);
+    async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        //await client.connect();
+        // Establish and verify connection
+        //await client.db("admin").command({ ping: 1 });
+        //console.log("Connected successfully to server");
+        console.log('Start the database stuff');
+        //Write databse Insert/Update/Query code here..
+        var dbo = client.db("DATAbased")
+        var myobj = { quoteName: n, salary: s, days: d }
+        await dbo.collection("quotes").insertOne(myobj, function(err, res) {
+            if (err) {
+                console.log(err);
+                throw err
+            }
+            console.log("1 quote inserted")
+        }); 
+        console.log('End the database stuff')
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close()
+    }
+    }
+    run().catch(console.dir)
+
+
+
+    res.send("stored "+n)
+})
+
+
+//this is for running the webpage
+//app.use(express.static(dir, options))
 
 
 
